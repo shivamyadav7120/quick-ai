5e392f54d1e6f43e066e159dbbe762a82796fe3d
shivamyadav7120 <shivamyadav7068257476@gmail.com>
2026-05-26 19:35:02 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const App = () => {
+  return (
+    <div>
+      <img src={viteLogo} alt="Vite logo" width="80"/>
+      <h1>App</h1>
+    </div>
+  )
+}
+export default App
